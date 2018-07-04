4f8e1b700414d9c654b81b289cb930a6e8b1b0cc
thanhhh <huythanh.cp12@gmail.com>
2018-07-04 15:49:34 +0700

    demo

--- demo.php
+++ demo.php
@@ -1,8 +1,18 @@
 <?php
-include_once ('model/db.class.php');
+$arr = array(2, 1, 3, 5, 4);
+$n = count($arr);
+for ($i = 0; $i < $n; $i++)
+{
+    $min = $i;
+    for ($j = $i; $j < $n; $j++){
+        if ($arr[$j] < $arr[$min]){
+            $min = $j;
+        }
+    }
 
-$result = $conn->query("select * from phuhuynh");
-
-while($row = $result->fetch(PDO::FETCH_ASSOC)) {
-    echo $row['ph_id'];
+    $temp = $arr[$i];
+    $arr[$i] = $arr[$min];
+    $arr[$min] = $temp;
 }
+print_r($arr);
+?>
